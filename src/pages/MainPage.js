2fd7177b1189fdce9583f4d9e1edd90660735dd4
kmujm <pmini09@naver.com>
2023-05-10 15:39:56 +0900

    fix: getAllInfo

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import Web3 from "web3";
 
 import backgorund from "../assets/background2.jpeg";
-import { ABI } from "../assets/abi";
+import { ABI } from "../assets/abiobj";
 
 const Container = styled.div`
   background-image: url(${backgorund});
@@ -15,6 +15,24 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+const Text = styled.div`
+  color: white;
+  margin: auto;
+  width: 40%;
+  height: 30px;
+  border-radius: 50px;
+  font-size: 4em;
+`;
+
+const SmallText = styled.div`
+  color: white;
+  margin: auto;
+  width: 40%;
+  height: 30px;
+  border-radius: 50px;
+  font-size: 1em;
+`;
+
 const AddInput = styled.input`
   margin: auto;
   display: block;
@@ -22,12 +40,13 @@ const AddInput = styled.input`
   height: 30px;
   border: none;
   border-radius: 10px;
+  margin-top: 100px;
 `;
 
 const Button = styled.button`
   margin: auto;
   display: block;
-  width: 10%;
+  min-width: 10%;
   height: 100px;
   background: none;
   border: none;
@@ -52,8 +71,13 @@ export const MainPage = () => {
   // 기록
   const [record, setRecord] = useState(0);
 
+  const [data, setData] = useState();
+
+  const [countDown, setCountDown] = useState();
+  const [paid, setPaid] = useState(false);
+
   // contract address
-  const address = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+  const address = "0xD0414937aeD63aC6bde8B0abd9E31Af040B65495";
 
   // contract instance
   const getInstance = async () => {
@@ -67,20 +91,31 @@ export const MainPage = () => {
 
   // game info, 상금, 최고기록 등 정보
   const getGameInfo = async () => {
-    return await contractInstance.methods.getGameInfo().call();
+    try {
+      const data = await contractInstance.methods.getAllInfo().call();
+      console.log(data);
+      setData(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
+  useEffect(() => {
+    if (data) {
+      Timer();
+    }
+  }, [data]);
+
   const checkGameState = async () => {
-    return await contractInstance.methods.checkGameOver().call();
+    return await contractInstance.methods.distributePrize().call();
     // if(result)
   };
 
   // contract instance 생성 후 game info get
   useEffect(() => {
     console.log(contractInstance);
-    const data = getGameInfo();
-    console.log(data);
-    checkGameState();
+    getGameInfo();
+    // checkGameState();
   }, [contractInstance]);
 
   // 최초 랜더링시 실행
@@ -113,33 +148,81 @@ export const MainPage = () => {
     console.log(account);
   };
 
+  // 참가비
+  const payFee = async () => {
+    try {
+      // 게임 참가비 송금
+      await contractInstance.methods.payEntryFee().send({
+        from: account,
+        value: window.web3.utils.toWei("0.0005", "ether"),
+      });
+      setPaid(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   // 게임 시작 버튼. ether 송금
   const handleGameStart = async () => {
     if (account && nickname) {
       console.log(account, nickname);
-      // 게임 참가비 송금
-
-      await contractInstance.methods
-        .play()
-        .send({
-          from: account,
-          value: window.web3.utils.toWei("0.001", "ether"),
-        });
-      navigate("/game", {
-        contractInstance: contractInstance,
-        account: account,
-      });
     } else if (!account) {
       alert("메타마스크를 연결해주세요");
     } else {
       alert("닉네임을 입력해주세요");
     }
+    try {
+      // 게임 참가비 송금
+      // await contractInstance.methods.payEntryFee().send({
+      //   from: account,
+      //   value: window.web3.utils.toWei("0.0005", "ether"),
+      // });
+      console.log(contractInstance, account, nickname);
+      navigate("/game", {
+        state: {
+          contractInstance: contractInstance,
+          account: account,
+          nickname: nickname,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const Timer = () => {
+    setInterval(convertTime, 1000);
+  };
+
+  const convertTime = () => {
+    const now = new Date().getTime();
+    const date = new Date(data[0] * 1000).getTime();
+    var diff = date - now;
+    const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const diffMin = Math.floor((diff / (1000 * 60)) % 60);
+    const diffSec = Math.floor((diff / 1000) % 60);
+    setCountDown({ day: diffDay, hour: diffHour, min: diffMin, sec: diffSec });
   };
 
   return (
     <Container>
+      {countDown ? (
+        <Text>
+          {countDown.day}일 {countDown.hour}시간 {countDown.min}분
+          {countDown.sec} 초
+        </Text>
+      ) : null}
+      {data ? (
+        <SmallText style={{ marginTop: 50 }}>
+          누적 {data[1]}명이 참가하였고 누적 상금은{" "}
+          {data[2] / 1000000000000000000} ether이며 최고기록은 {data[5]}{" "}
+          초입니다.
+        </SmallText>
+      ) : null}
       <AddInput onChange={handleInput} placeholder="닉네임을 입력해주세요" />
       {!account ? <Button onClick={connectWallet}>지갑 연결하기</Button> : null}
+      {/* {!paid ? <Button onClick={payFee}>참가비 내기</Button> : null} */}
       <Button onClick={handleGameStart}>Start Game!</Button>
     </Container>
   );
